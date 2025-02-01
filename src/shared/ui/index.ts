02c3fb6b8abead6288bd41c3d56f8af44b9aebbe
Yuriy Dolganov <jetscript@gmail.com>
2025-02-02 01:12:32 +0500

    Add re-export from ya.praktikum

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,12 @@
+export {
+  BurgerIcon,
+  Button,
+  ConstructorElement,
+  Counter,
+  CurrencyIcon,
+  DragIcon,
+  ListIcon,
+  Logo,
+  ProfileIcon,
+  Tab,
+} from '@ya.praktikum/react-developer-burger-ui-components';
