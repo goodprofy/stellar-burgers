e5aaf04b1671565e0f9050f45128f34d060e272f
Yuriy Dolganov <jetscript@gmail.com>
2025-02-01 21:50:24 +0500

    Add alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
+
+const srcPath = path.join(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(srcPath),
+      },
+    ],
+  },
 });
